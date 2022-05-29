4205eac116e0e26c8e6aa74b671b6b223250e2cc
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-05-29 22:25:26 +0300

    fixs

--- src/function.js
+++ src/function.js
@@ -1,78 +1,68 @@
+import './css/styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import pictureCard from './hbs/cards.hbs';
-import ApiService from './js/searcher.js';
-
+import API from './api/getPicters.js';
 
 const refs = {
     formEl: document.querySelector('.search-form'),
-    galleryEl: document.querySelector('.gallery'),
-     sentinel: document.querySelector('#sentinel'),
-};
-const apiService = new ApiService();
+    galleryEl: document.querySelector('.gallery'), 
+}
 
-function renderGallery(data) {
-    let markup = '';
-    data.hits.map(item => { markup += pictureCard(item) });
-  
-    refs.galleryEl.insertAdjacentHTML("beforeend", markup);
+async function renderGallery() {
+    const result = await API.getPicters();
+    appendPicters(result.data.totalHits);
+    
 }
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay:250,
     enableKeyboard: true,
 });
-lightbox.refresh();
+lightbox.refresh()
 
-refs.formEl.addEventListener('submit', onSearch);
+refs.formEl.addEventListener('submit', onSearch)
 
 function onSearch(e) {
     e.preventDefault();
     outputClear(); 
-    apiService.resetPage();
-    apiService.query = e.currentTarget.elements.searchQuery.value;
-        if (apiService.query.trim() === "") {
+    API.params.page = 1;
+    API.params.q = e.currentTarget.elements.searchQuery.value;
+        if (API.params.q.trim() === "") {
             Notiflix.Notify.failure('Please fill in the field');
             return;
         }
-    apiService.getPicters().then(({ data }) => {
+        renderGallery();
+    API.getPicters().then(({ data } = {}) => {
         appendPicters(data.hits);
         lightbox.refresh();
-        if (data.totalHits === 0) {
+        if (data?.totalHits === 0) {
             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');
             return;
         }        
-            if (data.totalHits !== 0) {
+            if (data?.totalHits !== 0) {
                 Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images!`);
         }       
     });    
 }   
-
 function appendPicters(card) {
     refs.galleryEl.insertAdjacentHTML('beforeend', pictureCard(card));
 }
-
 function outputClear() {
     refs.galleryEl.innerHTML = '';
 };   
 
-const onEntry = entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting && apiService.query !== '') {
-     apiService.incrementPage();
-        apiService.getPicters().then(({ data }) => {
-            appendPicters(data.hits);
+window.addEventListener("scroll", () => {
+    const docRect = document.documentElement.getBoundingClientRect();
+    if (docRect.bottom < document.documentElement.clientHeight + 150) {
+        API.params.page += 1;
+        API.getPicters().then(({ data } = {}) => {
+            appendPicters(data?.hits);
             lightbox.refresh();
-      });
+        });
     }
-  });
-};
-const observer = new IntersectionObserver(onEntry, {
-  rootMargin: '150px',
 });
-observer.observe(refs.sentinel);
-
 const offset = 700;
 const scrollUp = document.querySelector('.scroll-up');
 const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
