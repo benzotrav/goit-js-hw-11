0f0c94a9ed2fd7d461827226be10ff29db7fd5fe
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-05-29 22:27:54 +0300

    fix

--- src/js/searcher.js
+++ src/js/searcher.js
@@ -1,41 +1,28 @@
 import axios from 'axios';
-
 const URL = 'https://pixabay.com/api/';
-const API_KEY = '27695920-aa8882011fbc25a0de57751fe';
-export default class ApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-    }
-    async getPicters() {
-        const params = {
-            key: API_KEY,
-            q: this.searchQuery,
+const API_KEY = '27515523-9dca8758fab0b717270f23e63';
+
+const params = {
+            q: '',
             image_type: 'photo',
             orientation: 'horizontal',
             safesearch: 'true',
-            page: this.page,
+            page: 1,
             per_page: 40,
-        }
-     try {
-         const response = await axios.get(URL, {params})                  
-         this.incrementPage();
-               return response;             
-     }            
-     catch (error) {
-    console.error(error);
-  }
+            key: API_KEY,
+}
+const customAxios = axios.create({
+    baseURL: URL
+});
+const getPicters = async () => {
+    try {
+        const res = await customAxios.get('', { params })
+       
+        return res;
+        
+    } catch {
+        console.log('message');
     }
-    incrementPage() {
-        this.page += 1;
-    }
-    resetPage() {
-        this.page = 1;
-    }    
-    get query() {
-        return this.searchQuery;
-    }
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }  
-};
+}
+export default {params, getPicters}
+
